2b1cb339e363d34f9cf2885c770c9f5c97f935ce
bufgix <ooruc471@yandex.com>
2020-10-21 01:41:00 +0300

    feat: add download button

--- components/leftside/index.js
+++ components/leftside/index.js
@@ -2,14 +2,21 @@ import React from 'react'
 import styles from './index.module.css'
 
 import Header from '../nav'
-
+import {DownloadButton} from '../button'
 
 export default function LeftSide() {
   return (
     <div className={styles.leftSide}>
       <Header />
-      <img className={styles.bgImage} src="/images/bg-image.png" alt="bg-image" />
+      <img
+        className={styles.bgImage}
+        src="/images/bg-image.png"
+        alt="bg-image"
+      />
       <img className={styles.logo} src="/images/passwall-logo.png" alt="logo" />
+      <div className={styles.dowloadSection}>
+        <DownloadButton>MacOS</DownloadButton>
+      </div>
     </div>
   )
 }
