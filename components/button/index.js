26ffae983f95b3b9205b4743873f2c590a048991
bufgix <ooruc471@yandex.com>
2020-10-21 01:40:41 +0300

    style: add download button

--- components/button/index.js
+++ components/button/index.js
@@ -1,7 +1,15 @@
 import React from 'react'
 import cn from 'classnames'
 
-import styles from './index.module.css'
+import styles from './index.module.scss'
+
+export function DownloadButton({ children, className, ...props }) {
+  return (
+    <button type="button" className={cn(styles.downloadButton, className)} {...props}>
+      {children}
+    </button>
+  )
+}
 
 function Button({ children, className, ...props }) {
   return (
