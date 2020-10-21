3ed7514e9da077b8addcea04d28f940daba1947e
bufgix <ooruc471@yandex.com>
2020-10-21 10:59:12 +0300

    chore: change meta tag locations

--- pages/_document.js
+++ pages/_document.js
@@ -12,34 +12,6 @@ class MyDocument extends Document {
     return (
       <Html lang={SiteConfig.lang}>
         <Head>
-          {/* base */}
-          <meta charSet="utf-8" />
-          <meta httpEquiv="x-ua-compatible" content="ie=edge" />
-          <meta name="viewport" content="width=device-width, initial-scale=1" />
-          <meta name="description" content={SiteConfig.description} />
-
-          {/* facebook */}
-          <meta property="og:url" content={SiteConfig.siteUrl} />
-          <meta property="og:type" content="website" />
-          <meta property="og:title" content={SiteConfig.title} />
-          <meta property="og:description" content={SiteConfig.description} />
-
-          {/* twitter */}
-          <meta name="twitter:card" content="summary" />
-          <meta name="twitter:url" content={SiteConfig.siteUrl} />
-          <meta name="twitter:title" content={SiteConfig.title} />
-          <meta name="twitter:description" content={SiteConfig.description} />
-
-          {/* pwa */}
-          <link rel="manifest" href="/manifest.json" />
-          <meta name="theme-color" content="#fff" />
-          <meta name="application-name" content={SiteConfig.title} />
-          <meta name="apple-mobile-web-app-title" content={SiteConfig.title} />
-          <meta name="apple-mobile-web-app-capable" content="yes" />
-          <meta
-            name="apple-mobile-web-app-status-bar-style"
-            content="default"
-          />
           <link
             rel="apple-touch-icon"
             sizes="180x180"
